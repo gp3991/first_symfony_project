155af07ead1b9be16e88e437988f1948847a287b
gp3991 <gp3991@gmail.com>
2016-08-21 21:11:24 +0200

    template test added

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 21.08.16
+ * Time: 17:39
+ */
+
+namespace AppBundle\Controller;
+
+
+class HomeController {
+
+} 
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 21.08.16
+ * Time: 20:00
+ */
+
+namespace AppBundle\Entity;
+
+
+class User {
+
+} 
